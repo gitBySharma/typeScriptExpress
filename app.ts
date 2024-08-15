a249cae3adca816ca8c98591399e66d4c0573a00
gitBySharma <fetchsubhankar@gmail.com>
2024-08-15 14:46:59 +0530

    first commit

---
+++ app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import todosRoutes from './routes/todos';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(todosRoutes);
+
+app.listen(3000, () =>{
+    console.log("Server is live on port 3000");
+});
---
+++ routes/todos.js
@@ -0,0 +1,39 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const router = (0, express_1.Router)();
+let todos = [];
+router.get('/', (req, res, next) => {
+    res.status(200).json({ todos: todos });
+});
+router.post('/todo', (req, res, next) => {
+    const newTodo = {
+        id: new Date().toISOString(),
+        text: req.body.text
+    };
+    todos.push(newTodo);
+    res.status(200).json({ message: 'Todo added successfully', createdTodo: todos });
+});
+router.delete('/delete/:id', (req, res, next) => {
+    const id = req.params.id;
+    const index = todos.findIndex(todo => todo.id === String(id));
+    if (index !== -1) {
+        todos.splice(index, 1);
+        res.status(200).json({ message: 'Todo deleted successfully' });
+    }
+    else {
+        res.status(404).json({ message: 'Todo not found' });
+    }
+});
+router.put('/todo/edit/:id', (req, res, next) => {
+    const id = req.params.id;
+    const index = todos.findIndex(todo => todo.id === String(id));
+    if (index !== -1) {
+        todos[index].text = req.body.text;
+        res.status(200).json({ message: 'Todo updated successfully', updatedTodo: todos });
+    }
+    else {
+        res.status(404).json({ message: 'Todo not found' });
+    }
+});
+exports.default = router;
